dd47883d2d41bb7f0e68d4b0335845cf69fd0cd7
KhanmdNazrul <mdnazrulkhandu@gmail.com>
2024-06-09 13:32:02 +0600

    first commit

---
+++ 09 june 2024/php_syntx.php
@@ -0,0 +1,14 @@
+<?php
+//full syntax
+echo "Hello World<br>";
+
+?>
+
+<?
+//open short tag
+echo "Hello World With short tag<br>";
+?>
+
+<?="Hello World With short circuit";
+// short circuit
+?>
