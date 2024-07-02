555e6356ed04d92e4db1f71a3d63471d6cae3587
MD ARIF HASAN <mdarifhasan077@gmail.com>
2024-07-03 00:24:30 +0600

    02/07/24

---
+++ 02 july 2024/file_uploads.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_FILES['image'])){
+    $filename = $_FILES['image']['name'];
+    $filetmp = $_FILES['image']['tmp_name'];
+    move_uploaded_file($filetmp,"image/".$filename);
+    
+
+}
+
+
+?>
+<form method="post" enctype="multipart/form-data">
+    <input type="file" name ="image"/>
+    <input type="submit" value ="submit">
+</form>
