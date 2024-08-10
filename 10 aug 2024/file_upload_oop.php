38f1795c2217898f75d5efc9849040203d68ae6e
KhanmdNazrul <mdnazrulkhandu@gmail.com>
2024-08-10 13:35:59 +0600

    10/08/24

---
+++ 10 aug 2024/file_upload_oop.php
@@ -0,0 +1,25 @@
+<?php include_once "file_class.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_POST['submit'])) {
+        $info = ($_FILES['file']);
+        $obj = new File($info);
+        $obj->upload();
+    }
+    ?>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" name="submit" value="UPLOAD">
+    </form>
+</body>
+
+</html>
